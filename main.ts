99dff8159b09b71d28ef7d9ec95b037d6bb6d7c2
David12aldana <88721714+David12aldana@users.noreply.github.com>
2021-08-31 14:22:26 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,21 @@
+basic.forever(function () {
+	
+})
+basic.forever(function () {
+	
+})
+basic.forever(function () {
+    led.plot(2, 0)
+    led.plot(1, 1)
+    led.plot(0, 2)
+    led.plot(3, 1)
+    led.plot(4, 2)
+    led.plot(3, 4)
+    led.plot(1, 4)
+    led.plot(1, 3)
+    led.plot(3, 3)
+    led.plot(1, 2)
+    led.plot(2, 2)
+    led.plot(3, 2)
+    led.plot(2, 1)
+})
